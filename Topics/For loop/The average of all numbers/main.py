5faec9ec63da78b78087ea597e501c350717d52f
Andrey-Metelyov <andrey.metelyov@gmail.com>
2021-10-04 20:00:59 +0900

    The average of all numbers

---
+++ Topics/For loop/The average of all numbers/main.py
@@ -0,0 +1,10 @@
+# put your python code here
+a = int(input())
+b = int(input())
+sum = 0
+count = 0
+for i in range(a, b + 1):
+    if i % 3 == 0:
+        sum += i
+        count += 1
+print(sum / count)
