c84abffbf45ba1b0538d517e83d79c9b8db68f51
Andrey-Metelyov <andrey.metelyov@gmail.com>
2021-10-12 14:22:49 +0900

    Flower taxonomy in one string

--- Topics/Kwargs/Flower taxonomy/main.py
+++ Topics/Kwargs/Flower taxonomy/main.py
@@ -2,8 +2,8 @@ iris = {}
 
 
 def add_iris(id_n, species, petal_length, petal_width, **additional_features):
-    iris[id_n] = {'species': species, 'petal_length': petal_length, 'petal_width': petal_width}
-    iris[id_n].update(additional_features)
+    iris[id_n] = {'species': species, 'petal_length': petal_length, 'petal_width': petal_width, **additional_features}
+    # iris[id_n].update(additional_features)
 
 # add_iris(0, 'Iris versicolor', 4.0, 1.3, petal_hue='pale lilac')
 # print(iris)
