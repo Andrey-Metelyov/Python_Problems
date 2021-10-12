0c3b4cef18a21770ca8d8e59a7e67184334b6283
Andrey-Metelyov <andrey.metelyov@gmail.com>
2021-10-12 16:56:13 +0900

    A very nested list

---
+++ Topics/Nested lists/Accessing elements of a matrix/main.py
@@ -0,0 +1,12 @@
+# the variable M is already defined
+# M = [[34, 77, 8,  45],
+#      [10, 15, 93, 57],
+#      [78, 82, 19, 98]]
+
+print(M[2][0])
+
+# for i in range(len(M)):
+#     print(M[i][0], end=' ')
+#
+# for x in M[2]:
+#     print(x, end=' ')
